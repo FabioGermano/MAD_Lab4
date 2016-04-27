aebda3c8904cb09b3cd893b4b7edcf7446c48dcd
EugenioCardonatti <s222200@studenti.polito.it>
2016-04-27 20:08:50 +0200

    rimetto restaurantActivity (niente di che, lo cancello e lo riaggiungo)

---
+++ app/src/main/java/it/polito/mad_lab3/restaurant/RestaurantActivity.java
@@ -0,0 +1,52 @@
+package it.polito.mad_lab3.restaurant;
+
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import it.polito.mad_lab3.R;
+import it.polito.mad_lab3.restaurant.foodPhoto.PhotoViewer;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    PhotoViewer photo, photo2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+
+        collapsingToolbarLayout.setTitle("Demo");
+
+        setSupportActionBar(toolbar);
+
+        ((ImageView)findViewById(R.id.mapIcon)).setColorFilter(Color.BLACK);
+        ((ImageView)findViewById(R.id.phoneIcon)).setColorFilter(Color.BLACK);
+
+        photo = (PhotoViewer)getSupportFragmentManager().findFragmentById(R.id.photo1);
+        photo2 = (PhotoViewer)getSupportFragmentManager().findFragmentById(R.id.photo2);
+        photo.setImageByDrawable(R.drawable.cibo1);
+        photo2.setImageByDrawable(R.drawable.cibo2);
+
+        LinearLayout llOffers = (LinearLayout)findViewById(R.id.LLOffersPrev);
+        View child = getLayoutInflater().inflate(R.layout.offer_prev_view, null);
+        llOffers.addView(child);
+        child = getLayoutInflater().inflate(R.layout.offer_prev_view, null);
+        llOffers.addView(child);
+    }
+
+}
