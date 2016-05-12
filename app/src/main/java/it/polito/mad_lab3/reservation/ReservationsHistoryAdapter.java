cbeed20df4e822280733ea4e5131164f40e3fe0d
FabioGermano <gefasio@gmail.com>
2016-05-12 18:53:09 +0200

    memory leak problem risolto!

--- app/src/main/java/it/polito/mad_lab3/reservation/ReservationsHistoryAdapter.java
+++ app/src/main/java/it/polito/mad_lab3/reservation/ReservationsHistoryAdapter.java
@@ -109,9 +109,20 @@ public class ReservationsHistoryAdapter extends RecyclerView.Adapter<Reservation
             }
 
             private void goToRestaurantActivity() {
+
+                Reservation r = data.get(position);
+                Intent i= new Intent(context, RestaurantActivity.class);
+                Restaurant restaurant = RestaurantBL.getRestaurantById(context, r.getRestaurantId());
+                i.putExtra("idRestaurant", restaurant.getRestaurantId());
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
             }
 
             private void showOrderDetails() {
+
+                Reservation r = data.get(position);
+                DialogFragment newFragment = ReservationDetailsFragment.newInstance(Integer.parseInt(r.getPlaces()), r.getReservedDishes());
+                newFragment.show(activity.getFragmentManager(), "dialog");
             }
 
             private void removeReservation() {
@@ -164,3 +175,109 @@ public class ReservationsHistoryAdapter extends RecyclerView.Adapter<Reservation
         }
 
 }
+/*
+    private Context context;
+    ArrayList<Reservation> data;
+    Activity activity;
+
+    public ReservationsHistoryAdapter(Context context, Activity activity,  ArrayList<Reservation> objects) {
+        super(context, 0, objects);
+        this.context= context;
+        this.activity= activity;
+        this.data= objects;
+    }
+    private static class ViewHolder {
+
+        TextView restaurant, date, time, address, status;
+        Button info, restaurantPage, cancel;
+
+    }
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+
+        final Reservation r = getItem(position);
+        final Restaurant restaurant = RestaurantBL.getRestaurantById(context, r.getRestaurantId());
+
+        final ViewHolder viewHolder; // view lookup cache stored in tag
+
+        if (convertView == null) {
+
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.reservations_history_item, parent, false);
+
+            viewHolder.restaurant = (TextView)  convertView.findViewById(R.id.restaurant_name);
+            viewHolder.address = (TextView)  convertView.findViewById(R.id.address);
+            viewHolder.time = (TextView)  convertView.findViewById(R.id.time);
+            viewHolder.date = (TextView)  convertView.findViewById(R.id.date);
+            viewHolder.status = (TextView)  convertView.findViewById(R.id.status);
+
+            viewHolder.info = (Button)  convertView.findViewById(R.id.info);
+            viewHolder.restaurantPage = (Button)  convertView.findViewById(R.id.restaurant);
+            viewHolder.cancel = (Button) convertView.findViewById(R.id.cancelBtn);
+
+            // Get the data item for this position
+            convertView.setTag(viewHolder);
+
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.restaurant.setText(restaurant.getRestaurantName());
+        viewHolder.address.setText(restaurant.getBasicInfo().getAddress()+" - "+restaurant.getBasicInfo().getCity());
+        viewHolder.date.setText(r.getDate());
+        viewHolder.time.setText(r.getTime());
+        viewHolder.status.setText(r.getStatus());
+
+        if(r.getStatus().equals(ReservationTypeConverter.toString(ReservationType.ACCEPTED)) || r.getStatus().equals(ReservationTypeConverter.toString(ReservationType.PENDING)))
+            viewHolder.cancel.setVisibility(View.VISIBLE);
+        else
+            viewHolder.cancel.setVisibility(View.GONE);
+
+        viewHolder.info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO create dialogvoid showDialog() {
+                // Create the fragment and show it as a dialog.
+                DialogFragment newFragment = ReservationDetailsFragment.newInstance(Integer.parseInt(r.getPlaces()), r.getReservedDishes());
+                newFragment.show(activity.getFragmentManager(), "dialog");
+            }
+        });
+        viewHolder.cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(r.getStatus().equals(ReservationTypeConverter.toString(ReservationType.ACCEPTED))){
+                    // se in stato accettato
+                    UserBL.cancelReservation(UserBL.getUserById(getContext(), UserSession.userId),r.getReservationId(), false);
+
+                }
+                else if(r.getStatus().equals(ReservationTypeConverter.toString(ReservationType.PENDING))){
+                    UserBL.cancelReservation(UserBL.getUserById(getContext(), UserSession.userId),r.getReservationId(), true);
+                }
+
+                Toast.makeText(getContext(), R.string.reservation_canceled, Toast.LENGTH_SHORT );
+
+            }
+        });
+        viewHolder.restaurantPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(context, RestaurantActivity.class);
+                i.putExtra("idRestaurant", restaurant.getRestaurantId());
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            }
+        });
+
+
+        return convertView;
+    }
+
+
+
+
+
+}
+*/
